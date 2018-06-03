8eea0987859c14e47c6c5056e4a34c39288fdb48
Paulo Oliveira <paulo.oliveira278@gmail.com>
2018-06-03 19:46:19 -0300

    app

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,33 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  message: string = 'Pull down to refresh';
-  items: number[] = [1, 2, 3, 4, 5];
+  loading: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  onAction($event) {
-    setTimeout(() => {
-      $event.done();
-      this.items.push(0);
-    }, 1000);
-  }
-
-  onChangeState($event) {
-    switch ($event.state) {
-      case 'initial':
-        this.message = 'Pull down to refresh';
-        break;
-      case 'preaction':
-        this.message = 'Release to refresh';
-        break;
-      case 'action':
-        this.message = 'Loading data...';
-        break;
-    }
-  }  
-
 }
