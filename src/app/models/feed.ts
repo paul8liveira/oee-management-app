f920e4ca4d2c9aaab3cd846d1345bb078961b68e
paul8liveira <paulo.oliveira278@gmail.com>
2018-06-21 00:20:10 -0300

    app

--- src/app/models/feed.ts
+++ src/app/models/feed.ts
@@ -4,8 +4,17 @@ export class Feed extends BaseModel {
   id: number;
   ch_id: number;
   mc_cd: string;
+  mobile_name: string;
+  field1: string;
   field2: number;
+  field3: number;
   field4: number;
+  field5: string;
+  field1_desc: string;
+  field2_desc: string;
+  field3_desc: string;
+  field4_desc: string;
+  field5_desc: string;
   time: string;
   oee: string;
   
