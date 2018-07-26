c1b902fa6054ff5bc9284c771cb7fd73efe62e24
paul8liveira <paulo.oliveira278@gmail.com>
2018-07-25 23:13:11 -0300

    new version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 const serverURL = 'http://localhost:3000/oee/api/';
 export const environment = {
   production: false,
-  version: '1.0.0',
+  version: '0.0.1',
   userAuthenticationURL: serverURL + 'user/authentication',
   feedURL: serverURL + ':userId/:channelId/:machineCode/:date/:limit/feed/mobile',
   userAddURL: serverURL + 'user',
