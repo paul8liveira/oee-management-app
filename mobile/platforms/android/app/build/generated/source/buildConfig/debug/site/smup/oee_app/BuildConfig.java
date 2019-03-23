660b5788e478b41c2e7c759a294a18e4dc07569f
paul8liveira <paul8liveira@gmail.com>
2019-03-23 12:47:00 -0300

    adicionando platforma a estrutura

---
+++ mobile/platforms/android/app/build/generated/source/buildConfig/debug/site/smup/oee_app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package site.smup.oee_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "site.smup.oee_app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "0.0.9";
+}
