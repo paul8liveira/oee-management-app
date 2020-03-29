8a37f4dcbe8a803e30c69b9c3575eee6573008d4
Paulo Oliveira <paulooliveira@pecege.com>
2020-03-29 11:01:59 -0300

    v0.0.12

--- mobile/platforms/android/app/build/generated/source/buildConfig/release/site/smup/oee_app/BuildConfig.java
+++ mobile/platforms/android/app/build/generated/source/buildConfig/release/site/smup/oee_app/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "site.smup.oee_app";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 11;
-  public static final String VERSION_NAME = "0.0.11";
+  public static final int VERSION_CODE = 12;
+  public static final String VERSION_NAME = "0.0.12";
 }
